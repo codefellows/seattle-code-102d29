29812554462264704ed46ed740abb882e95776c7
Roger Huba <rogerhuba@gmail.com>
2021-07-28 16:59:13 -0500

    add class 6 demo

---
+++ class-06/join_empire/app1.js
@@ -0,0 +1,18 @@
+console.log('Hello World!')
+
+let userName = prompt('What is your Name?')
+
+if(userName.toLowerCase() == 'roger'){
+    document.write('<h2>Welcome Lord ' + userName + '</h2>')
+} else {
+    document.write('<h2>Welcome ' + userName + '</h2>')
+}
+
+
+
+// Data Types
+
+// string: Letters or numbers
+// number or integer
+// Boolean True or a False
+// Null
---
+++ class-06/join_empire/app2.js
@@ -0,0 +1,8 @@
+let userAnswer = prompt('Do you want to join the Empire?')
+
+if (userAnswer.toLowerCase() == 'yes'){
+    document.write('APPLY')
+}
+
+today = Date()
+console.log(today)
